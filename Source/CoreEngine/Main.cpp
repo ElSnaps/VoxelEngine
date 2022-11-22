e7827dcb663c55e56a506392b4199d4fef6ef0bf
Sunny Blake-Webber <9282017+ElSnaps@users.noreply.github.com>
2022-11-22 01:29:29 +0000

    Upgrade CMake files, Added EntryPoint

---
+++ Source/CoreEngine/Main.cpp
@@ -0,0 +1,17 @@
+// Copyright Snaps 2022. All Rights Reserved.
+
+#include "Engine.h"
+
+#include <iostream>
+
+/*
+	Main handles all our entry points for launching into our application on various
+	platform architectures, Aka windows or linux entry points will exist in here.
+*/
+
+int main()
+{
+	std::cout << "Test";
+	std::cin.get();
+    return 0;
+}
--- Source/main.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Snaps 2022. All Rights Reserved.
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "test \n";
-    return 0;
-}
